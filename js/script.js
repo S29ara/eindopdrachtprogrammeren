f19e16741885ba1c9c582e96c5e85a060d0c049a
S29ara <118111104+S29ara@users.noreply.github.com>
2023-03-24 19:23:07 +0100

    aangepast

--- js/script.js
+++ js/script.js
@@ -24,7 +24,7 @@ var textCloud = document.querySelector(".textcloud");
 
 var randomButton = document.querySelector(".random_button")
 
-var damageArray = ["images/meangirl_blood.png", "images/meangirl_blue_eye.png", "images/meangirl_trash.png", "images/meangirl_onfire.png"];
+var damageArray = ["/images/meangirl_blood.png", "/images/meangirl_blue_eye.png", "/images/meangirl_trash.png", "/images/meangirl_onfire.png"];
 
 var fewClicks = 0;
 
